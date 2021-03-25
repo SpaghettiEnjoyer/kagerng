8d35af1e1399a912f22454145b56da42fd0c0463
aMehGit <70306382+aMehGit@users.noreply.github.com>
2021-03-25 05:39:36 +0100

    Update listen.js

--- overlay/lib/listen.js
+++ overlay/lib/listen.js
@@ -18,11 +18,11 @@ let index60 = 0;
 function initSfx() {
   if (goodRngSfx === undefined) {
     const goodRngSfxConfig = config.get('sfx.good_rng');
-    goodRngSfx = new Howl({src: ["https://downloads.khinsider.com/game-soundtracks/album/legend-of-zelda-ocarina-of-time-original-sound-track/01%2520-%2520Title%2520Theme.mp3"], volume: goodRngSfxConfig.volume, loop: goodRngSfxConfig.loop});
+    goodRngSfx = new Howl({src: ["https://vgmsite.com/soundtracks/legend-of-zelda-ocarina-of-time-original-sound-track/twginset/01%20-%20Title%20Theme.mp3"], volume: goodRngSfxConfig.volume, loop: goodRngSfxConfig.loop});
   }
   if (badRngSfx === undefined) {
     const badRngSfxConfig = config.get('sfx.bad_rng');
-    badRngSfx = new Howl({src: ["https://downloads.khinsider.com/game-soundtracks/album/legend-of-zelda-ocarina-of-time-original-sound-track/01%2520-%2520Title%2520Theme.mp3"], volume: badRngSfxConfig.volume, loop: badRngSfxConfig.loop});
+    badRngSfx = new Howl({src: ["https://vgmsite.com/soundtracks/legend-of-zelda-ocarina-of-time-original-sound-track/twginset/01%20-%20Title%20Theme.mp3"], volume: badRngSfxConfig.volume, loop: badRngSfxConfig.loop});
   }
 }
 
